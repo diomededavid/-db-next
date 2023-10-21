d90cdaefc3b8e4dbbf4c56f409d9146078655664
diomededavid <david@d2creativedesign.com>
2023-10-21 09:00:20 -0700

    adds prettier config and css utilities

---
+++ components/NavMenu.tsx
@@ -0,0 +1,15 @@
+interface NavMenuProps {
+    /**
+     * Proop
+     */
+    prop: string;
+}
+export const NavMenu = ({}:NavMenuProps) => {
+    return(
+        <div>
+            NavMenu
+        </div>
+    )
+};
+
+export default NavMenu;
